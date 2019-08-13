e591b3a47467dc9421d673aad76e176721ae0cc3
Danilo Martinelli <danilomartinelli429@gmail.com>
2019-08-13 10:04:15 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew upgrade --all
+brew upgrade
 brew install mas
     
 # Install Apps
